430e6cd2a04fca28095977215002883a0de3b390
Mahule Roy <125608791+dreamboat26@users.noreply.github.com>
2024-12-14 12:23:07 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,11 +1,3 @@
-'''
- # @ Author: Meet Patel
- # @ Create Time: 2024-11-07 05:30:00
- # @ Modified by: Meet Patel
- # @ Modified time: 2024-11-09 09:06:22
- # @ Description:
- '''
-
 from torcheval.metrics.functional import peak_signal_noise_ratio
 from torchmetrics.image import StructuralSimilarityIndexMeasure
 
