ab47613b4538a31e81f823a859ada81709a8ac8f
Mahule Roy <125608791+dreamboat26@users.noreply.github.com>
2024-12-14 12:23:26 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,3 @@
-'''
- # @ Author: Meet Patel
- # @ Create Time: 2024-11-07 10:32:00
- # @ Modified by: Meet Patel
- # @ Modified time: 2024-11-09 09:07:17
- # @ Description:
- '''
-
 import torch
 import torch.nn as nn
 
